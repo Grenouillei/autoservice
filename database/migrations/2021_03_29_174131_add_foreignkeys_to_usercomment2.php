94cac2886890f1cb9b3f543381d43560eaad07e9
Grenouillei <yulik15zaytpa@gmail.com>
2021-03-29 21:20:57 +0300

    relationship v.4

---
+++ database/migrations/2021_03_29_174131_add_foreignkeys_to_usercomment2.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignkeysToUsercomment2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_comments', function (Blueprint $table) {
+            $table->foreign('id_user')->references('id')->on('users')
+                                    ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_comments', function (Blueprint $table) {
+            //
+        });
+    }
+}
